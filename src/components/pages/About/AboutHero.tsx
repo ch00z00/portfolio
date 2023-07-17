bb74bce7886f13635823a51c7333ab52b607b257
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-07-18 08:48:14 +0900

    [add] Added original my head model into the about page & installed GSAP

--- src/components/pages/About/AboutHero.tsx
+++ src/components/pages/About/AboutHero.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
+import { HeadModel } from "../../../../public/models/head";
+import { Canvas } from "@react-three/fiber";
+import { Float, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 
 export const AboutHero: React.FC = () => {
   return (
-    <div className="h-screen w-screen bg-black-100 pt-20 text-white-200">
-      <div className="grid grid-cols-2 items-center justify-center px-40">
-        <div className="flex h-2/3 w-full flex-col items-start justify-center gap-12 bg-black-100 pl-10 font-syne">
-          <p className="font-orbitron text-xl">01.</p>
-          <div className="ml-8">
-            <p className="mb-8 text-7xl">Hello</p>
-            <p className="font-neuropol text-lg">
-              Hi guys 👋 My name is Yusuke Seki. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro, magni laborum voluptatem
-              praesentium at voluptate, officia ipsa, veritatis iste in vitae
-              nemo minima ipsum repellendus doloremque impedit temporibus
-              ratione neque.
-            </p>
-          </div>
-        </div>
-        <Image src="/assets/me.png" width={700} height={700} alt="me" />
-      </div>
+    <div className="h-screen w-screen bg-black-200 text-white-200">
+      <Canvas camera={{ fov: 10, position: [0, 0, 10] }}>
+        <color attach="background" args={["white"]} />
+        <ambientLight />
+        <OrbitControls />
+        <Suspense>
+          <Float floatIntensity={1} rotationIntensity={0}>
+            <HeadModel />
+          </Float>
+        </Suspense>
+      </Canvas>
     </div>
   );
 };
