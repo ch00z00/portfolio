dc1731aae33e3747a14564ebfb4f401335627e3c
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-07-22 00:57:02 +0900

    [fix] Refactored head model (This Mark II is still a prototype and subject to change.)

--- src/components/pages/About/AboutHero.tsx
+++ src/components/pages/About/AboutHero.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { HeadModel } from "../../../../public/models/head";
 import { Canvas } from "@react-three/fiber";
 import { Float, OrbitControls } from "@react-three/drei";
@@ -8,9 +7,9 @@ export const AboutHero: React.FC = () => {
   return (
     <div className="h-screen w-screen bg-black-200 text-white-200">
       <Canvas camera={{ fov: 10, position: [0, 0, 10] }}>
-        <color attach="background" args={["white"]} />
+        <color attach="background" args={["black"]} />
         <ambientLight />
-        <OrbitControls />
+        <OrbitControls enableZoom={false} />
         <Suspense>
           <Float floatIntensity={1} rotationIntensity={0}>
             <HeadModel />
