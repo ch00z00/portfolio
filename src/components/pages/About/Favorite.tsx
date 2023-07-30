f2550fe86695e16a718e6a73d3168f46455a4bf7
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-07-30 23:21:45 +0900

    [add] Added some scroll-trigger effects using GSAP.

--- src/components/pages/About/Favorite.tsx
+++ src/components/pages/About/Favorite.tsx
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 /** TODO:
  * [title]: Implement an slide in animation only once when loaded.
  * [items]: Implement an type effect only once when loaded & a cursor sticking effect when hovering.
  */
 
 export const Favorite: React.FC = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      gsap.registerPlugin(ScrollTrigger);
+      setAnimation();
+    }
+  }, []);
+
+  const setAnimation = () => {
+    gsap.fromTo(
+      "#slideX p",
+      { alpha: 0, x: 30 },
+      {
+        alpha: 1,
+        x: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: "slideX",
+          start: "top 100%",
+          toggleActions: "play none none reset",
+        },
+      }
+    );
+  };
+
   return (
     <div className="mx-auto flex h-screen w-screen flex-col-reverse bg-white-200 py-52 text-center text-black-200 md:grid md:grid-cols-2 md:py-0 md:pl-40 md:text-start xl:pl-60">
       <div className="flex flex-col justify-center gap-14 pt-32 font-orbitron text-lg font-light tracking-wider md:gap-28 md:pt-0 xl:text-3xl">
@@ -11,9 +39,12 @@ export const Favorite: React.FC = () => {
         <p>Watching movies 🎥🍿</p>
         <p>Work out 🏋️💪</p>
       </div>
-      <div className="flex flex-col justify-center gap-8 md:items-start">
+      <div
+        className="flex flex-col justify-center gap-8 md:items-start"
+        id="slideX"
+      >
         <p className="font-orbitron text-sm lg:text-xl">04.</p>
-        <h1 className="font-syne text-4xl lg:text-6xl">Favorite</h1>
+        <p className="font-syne text-4xl lg:text-6xl">Favorite</p>
       </div>
     </div>
   );
--- src/components/pages/About/Skills.tsx
+++ src/components/pages/About/Skills.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 /** TODO:
  * [title]: Implement an up-to-down slide in animation only once when loaded.
  * [sub-title]: Implement an animation that performs the slide mark only once when loaded.
@@ -5,28 +9,84 @@
  */
 
 export const Skills: React.FC = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      gsap.registerPlugin(ScrollTrigger);
+
+      setAnimation();
+    }
+  }, [ref]);
+
+  const setAnimation = () => {
+    const stagger = 0.05;
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ref.current,
+          start: "top 100%",
+          toggleActions: "play none none reset",
+        },
+      })
+      .to(ref.current, {
+        scrollTrigger: {
+          trigger: ref.current,
+          start: "top center",
+          once: true,
+        },
+        opacity: 1,
+        duration: 1,
+        stagger,
+      })
+      .fromTo(
+        "#slideY p",
+        { opacity: 0, y: 30 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.4,
+          stagger,
+          scrollTrigger: {
+            trigger: ref.current,
+            start: "top 100%",
+            toggleActions: "play none none reset",
+          },
+        }
+      );
+  };
+
   return (
-    <div className="h-screen w-screen bg-blue-200 pt-32 text-white-200">
-      <div className="mb-20 flex flex-col items-center justify-center gap-8">
+    <div
+      className="h-screen w-screen bg-blue-200 pt-32 text-white-200"
+      ref={ref}
+    >
+      <div
+        className="mb-20 flex flex-col items-center justify-center gap-8"
+        id="slideY"
+      >
         <p className="font-orbitron text-sm md:text-xl">03.</p>
-        <h1 className="font-syne text-4xl md:text-6xl">Skills</h1>
+        <p className="font-syne text-4xl md:text-6xl">Skills</p>
       </div>
-      <div className="grid grid-cols-1 justify-center gap-14 font-neuropol text-2xl tracking-wider md:grid-cols-2 md:gap-0 md:px-40 xl:text-4xl">
+      <div
+        className="grid grid-cols-1 justify-center gap-14 font-neuropol text-2xl tracking-wider md:grid-cols-2 md:gap-0 md:px-40 xl:text-4xl"
+        id="slideX"
+      >
         <div className="mx-auto text-center">
           TECK STACKS
-          <div className="flex flex-col gap-3 pt-5 font-orbitron text-base md:gap-6 md:pt-14 xl:text-2xl">
-            <p>Next.js (React)</p>
-            <p>TypeScript</p>
-            <p>WebGL, Three.js</p>
-            <p>TailwindCSS, GSAP</p>
-          </div>
+          <ul className="flex flex-col gap-3 pt-5 font-orbitron text-base md:gap-6 md:pt-14 xl:text-2xl">
+            <li>Next.js (React)</li>
+            <li>TypeScript</li>
+            <li>WebGL, Three.js</li>
+            <li>TailwindCSS, GSAP</li>
+          </ul>
         </div>
         <div className="mx-auto text-center">
           OTHER
-          <div className="flex flex-col gap-3 pt-5 font-orbitron text-base md:gap-6 md:pt-14 xl:text-2xl">
-            <p>DL (MT)</p>
-            <p>TOEIC 660</p>
-          </div>
+          <ul className="flex flex-col gap-3 pt-5 font-orbitron text-base md:gap-6 md:pt-14 xl:text-2xl">
+            <li>DL (MT)</li>
+            <li>TOEIC 660</li>
+          </ul>
         </div>
       </div>
     </div>
