1e114ee518a0e62d46ca1eaca2fc0c1f9caa2fa2
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-08-07 05:38:07 +0900

    [comment] Commented about TODO.

--- src/components/elements/Button/LinkButton.tsx
+++ src/components/elements/Button/LinkButton.tsx
@@ -16,8 +16,8 @@ export const LinkButton: React.FC<LinkButtonProps> = ({ href, text }) => {
         <Link
           href={href}
           className={clsx(
-            "relative font-orbitron text-xs tracking-widest text-white-200 before:absolute before:bottom-0 before:left-0 before:block before:h-[2px] before:w-full before:origin-top-left before:scale-x-0 before:bg-white-200 before:transition before:duration-1000 before:ease-in-out before:content-[''] before:hover:scale-x-100 sm:text-sm"
-            // TODO: bg-white-200 になったら text-black-100 にする
+            "relative font-orbitron text-xs tracking-widest text-white-200 mix-blend-exclusion before:absolute before:bottom-0 before:left-0 before:block before:h-[2px] before:w-full before:origin-top-left before:scale-x-0 before:bg-white-200 before:transition before:duration-1000 before:ease-in-out before:content-[''] before:hover:scale-x-100 sm:text-sm"
+            // TODO: bg-white-200 になったら text-black-100 と before:bg-black-100 にする filter:invert filter:grayscale filter:contrast-100
           )}
         >
           {text}
