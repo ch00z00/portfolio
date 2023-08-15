49c81eb972f33690f315a4f446e540f3e36361d1
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-08-15 23:58:44 +0900

    [fix] Fixed home page.

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -3,11 +3,17 @@ import Image from "next/image";
 export const HomePage: React.FC = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black-100">
+      <span className="z-10 font-neuropol text-8xl text-white-200">
+        Optimistic
+        <br /> <span className="text-5xl">&</span> <br />
+        sociopathic
+      </span>
       <Image
         src="/assets/pom.jpeg"
         width={400}
         height={400}
         alt="picture of me"
+        className="-ml-10"
       />
     </div>
   );
