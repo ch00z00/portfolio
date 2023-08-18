d6b4bb1750e32940c485613c673a6135905a205a
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-08-18 09:42:40 +0900

    [add] Installed & set up Husky & lint-staged.

--- src/components/pages/Blog/article/ArticleBody.tsx
+++ src/components/pages/Blog/article/ArticleBody.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const ArticleBody: React.FC<Props> = ({ article }) => {
   return (
     <>
-      <div className="mx-auto h-fit w-2/3 overflow-hidden rounded-3xl bg-white-200/90 px-7 py-12 backdrop-blur-sm backdrop-filter md:px-20 md:py-28">
+      <div className="mx-auto h-fit w-2/3 overflow-hidden rounded-3xl bg-white-200/90 px-7 py-12 backdrop-blur-sm md:px-20 md:py-28">
         <div className="post">
           <div
             className="prose prose-sm font-orbitron tracking-wide md:prose-xl"
