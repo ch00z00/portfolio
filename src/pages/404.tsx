5dfab2137108d0cc8128c541f98d9ffaaed5e5f1
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2023-08-21 14:43:50 +0900

    [add] Added custom 404 page.

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import { AnimLinkButton } from "@/components/elements/Button";
+import { RevealText } from "@/components/elements/Text";
+
+export default function Custom404() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-8 bg-black-100">
+      <RevealText
+        text="404 - Page Not Found"
+        className="font-earth text-2xl tracking-tighter text-yellow-100 md:text-3xl lg:text-5xl"
+      />
+      <AnimLinkButton href="/" className="w-[300px] font-neuropol sm:w-[450px]">
+        <div className="flex items-center justify-between text-yellow-100">
+          <div>Back to the HOME</div>
+          <div className="rounded-full bg-black-200 px-3 text-white-200">→</div>
+        </div>
+      </AnimLinkButton>
+    </div>
+  );
+}
