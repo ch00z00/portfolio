828cee60e462ad5262aa0c597ef9643e70acdf5c
Yusuke Seki <Yusuke1@YusukenoMBP-3.AirPort>
2023-08-29 23:55:54 +0900

    [fix] Changed some styles.

--- src/components/layouts/Nav/Navigator.tsx
+++ src/components/layouts/Nav/Navigator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { Header } from "../Header";
 import { Menu } from "./Menu";
@@ -7,6 +7,7 @@ export const Navigator: React.FC = () => {
   const hamRef = useRef<HTMLDivElement>(null);
   const menuRef = useRef<HTMLUListElement>(null);
   const linksRef = useRef<NodeListOf<HTMLLIElement>>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const ham = hamRef.current;
@@ -16,7 +17,7 @@ export const Navigator: React.FC = () => {
     const tl = gsap.timeline({ paused: true });
 
     tl.to(menu, {
-      duration: 1,
+      duration: 0.7,
       opacity: 1,
       height: "100vh",
       ease: "power4.inout",
@@ -25,25 +26,32 @@ export const Navigator: React.FC = () => {
     tl.from(
       links,
       {
-        duration: 1,
+        duration: 0.5,
         opacity: 0,
         y: 20,
         stagger: 0.1,
         ease: "power4.inout",
       },
-      "-=0.5"
+      "<"
     );
 
     tl.reverse();
 
-    ham?.addEventListener("click", () => {
+    const handleClick = () => {
+      setIsMenuOpen((prevState) => !prevState);
       tl.reversed(!tl.reversed());
-    });
+    };
+
+    ham?.addEventListener("click", handleClick);
+
+    return () => {
+      ham?.removeEventListener("click", handleClick);
+    };
   }, []);
 
   return (
     <nav className="fixed top-8 z-10 flex w-screen items-center justify-between px-10 sm:px-16">
-      <Header hamRef={hamRef} />
+      <Header hamRef={hamRef} isMenuOpen={isMenuOpen} />
       <Menu menuRef={menuRef} />
     </nav>
   );
