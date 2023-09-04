61a7fe4c09dd41fa976886be26433251f2ab4a0c
Yusuke Seki <Yusuke1@YusukenoMBP-3.AirPort>
2023-09-05 01:05:44 +0900

    [fix] The sentences used in HOME are now used together as const in constants.

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -9,11 +9,40 @@ export const DESCRIPTION = {
 };
 
 export const HOME_01_SENT_EN =
-  "I began studying IT around my 2nd year of university, and for about 2 years from that point, I worked as a long-term intern as a front-end engineer at a domestic IT startup company in Japan. However, during my time there, I had a moment of reflection about my own market value in the industry. So, I decided to temporarily part ways with the company and contemplated my worth as an engineer. During this period of introspection, I stumbled upon Kenta Toshikura's portfolio website, where I was drawn to the sophisticated design and development of websites incorporating 3D elements and clever design choices. Simultaneously, I found clues to discover my own market value. Since then, I have aspired to become an engineer capable of creating exceptional websites akin to those found overseas.";
+  "I began studying IT around my 2nd year of university, and for about 2 years from that point, I worked as a long-term intern as a front-end engineer at a domestic IT startup company in Japan. \n However, during my time there, I had a moment of reflection about my own market value in the industry. So, I decided to temporarily part ways with the company and contemplated my worth as an engineer. \n During this period of introspection, I stumbled upon Kenta Toshikura's portfolio website, where I was drawn to the sophisticated design and development of websites incorporating 3D elements and clever design choices. Simultaneously, I found clues to discover my own market value. \n Since then, I have aspired to become an engineer capable of creating exceptional websites akin to those found overseas.";
 export const HOME_01_SENT_JP =
-  "私は大学2年生頃からITを学び始め、同年から2年ほど日本国内のITベンチャー企業で長期インターン生のフロントエンドエンジニアとして働きました。しかし、そこで働く中で、業界における自身の市場価値について考えるきっかけがありました。そこで一度某企業を離れ、エンジニアとしての存在価値を考えました。その最中に利倉健太さんのポートフォリオサイトに出会い、3D表現や巧みなデザインを駆使した洗練されたウェブサイトのデザイン・開発に惹かれ始めました。それと同時に、自分の市場価値を見出すヒントを得ました。それから私は海外のサイトのような優れたウェブサイトを生み出せるエンジニアになりたいと思うようになりました。";
+  "私は大学2年生頃からITを学び始め、同年から2年ほど日本国内のITベンチャー企業で長期インターン生のフロントエンドエンジニアとして働きました。\n しかし、そこで働く中で、業界における自身の市場価値について考えるきっかけがありました。そこで一度某企業を離れ、エンジニアとしての存在価値を考えました。\n その最中に利倉健太さんのポートフォリオサイトに出会い、3D表現や巧みなデザインを駆使した洗練されたウェブサイトのデザイン・開発に惹かれ始めました。それと同時に、自分の市場価値を見出すヒントを得ました。\n それから私は海外のサイトのような優れたウェブサイトを生み出せるエンジニアになりたいと思うようになりました。";
+
+export const HOME_03_SENT_EN = [
+  {
+    title: "Watching Films",
+    sent: "I like movies because they make me reevaluate my values and my life, and they make me think about what I would do in the situation.\n I usually watch movies mainly on Amazon Prime video.\n If you are a movie lover, let's talk about it together.",
+  },
+  {
+    title: "Working out",
+    sent: "I go to the university gym 7 days a week to work out.\n My shoulders were narrow because my collarbone was not that long, and I had well-developed trapezius muscles, so I had stroked shoulders and no matter how much I ate, I never gained weight and was skinny. At first I hated my body like that because it was so ugly and lame, so I started working out.\n However, as I trained more and more, training itself became fun, and the sense of accomplishment and euphoria after training made my body need to do muscle training.",
+  },
+  {
+    title: "Making yogurt",
+    sent: "One day, in my spare time, I learned that I could make homemade yogurt and started making yogurt.\n At first, I thought it was super-cost-effective to multiply yogurt by 5~6 times with milk, but I began to experiment to make the best yogurt ever by changing the seed yogurt, temperature when warming, fermentation time, etc.\n Incidentally, at this point, the best yogurt was made by using Meiji's R-1 as the seed, microwaving it at 500W for 2 minutes, and fermenting it for 12 hours.",
+  },
+];
+export const HOME_03_SENT_JP = [
+  {
+    title: "映画鑑賞",
+    sent: "映画は自分の価値観や人生を見直すきっかけになったり、自分ならこの状況でどうするかなどを考えさせられるので好きです。\n 普段は主にAmazon Prime videoで映画を観ています。\n 映画好きの方はぜひ一緒に語らいましょう〜。",
+  },
+  {
+    title: "筋トレ",
+    sent: "私は週7で大学ジムに通って筋トレをしています。\n 自分はもともと骨格的に鎖骨がそこまで長くないので肩幅が狭く、僧帽筋が発達していたため撫で肩で、体質的にいくら食べても全く太らずガリガリでした。はじめはそんな自分の体がダサくて嫌いだったので鍛え始めました。\n しかし、トレーニングを重ねるにつれてトレーニングそれ自体が楽しいことや、トレ後の達成感や多幸感で筋トレをしなければいけない体になってしまいました。",
+  },
+  {
+    title: "ヨーグルト作り",
+    sent: "ある時、暇を持て余した私は「自家製ヨーグルトが作れる」という情報を知り、ヨーグルトを作り始めました。\n 当初は「ヨーグルトを牛乳で5~6倍の量に増殖できるって超コスパええやん」と思って作っていましたが、種となるヨーグルトを変えてみたり、温める際の温度を変えてみたり、発酵時間を変えてみたりというように自分史上最高のヨーグルトを作るための実験として嗜むようになりました。\n ちなみに現時点では、明治のR-1を種とし、500Wで2分レンチンし、12時間発酵させたヨーグルトがベストでした。",
+  },
+];
 
 export const HOME_04_SENT_EN =
-  "I'm basically an optimistic and sociable individual with a touch of a sociopath personality trait. Additionally, I'm minimalist, so I don't have much of an appetite for things.";
+  "I'm basically an optimistic and sociable individual with a touch of a sociopath personality trait. \n Additionally, I'm minimalist, so I don't have much of an appetite for things.";
 export const HOME_04_SENT_JP =
-  "わたしは基本的に楽観主義者で社会性のあるソシオパス気質の人間です。また、ミニマリストでもあるため、あまり物欲がありません。";
+  "わたしは基本的に楽観主義者で社会性のあるソシオパス気質の人間です。\n また、ミニマリストでもあるため、あまり物欲がありません。";
