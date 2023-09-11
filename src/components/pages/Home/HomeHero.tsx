89aa9de9c530df2cdb4b537ff2290f321d81e951
Yusuke Seki <Yusuke1@YusukenoMBP-3.AirPort>
2023-09-12 05:45:03 +0900

    [fix] Modified homehero responsive & fixed contact section.

--- src/components/pages/Home/HomeHero.tsx
+++ src/components/pages/Home/HomeHero.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import MediaQuery from "react-responsive";
 
-import { RevealText } from "@/components/elements/Text";
+import { RevealText, STRevealText } from "@/components/elements/Text";
 import { Parallax } from "react-scroll-parallax";
 import { ExpCircle } from "@/components/elements/Expelimental";
 
@@ -13,17 +13,20 @@ export const HomeHero: React.FC = () => {
     setIsClient(true);
   }, []);
   return (
-    <div className="relative flex h-fit w-screen flex-col items-center bg-black-100 pb-96 pt-36">
+    <div className="relative flex h-fit w-screen flex-col items-center bg-black-100 pb-[min(26vh)] pt-36 sm:pb-[min(30vh)] md:pb-[min(60vh)]">
       {isClient ? (
         <>
           <Parallax
-            speed={-10}
-            className="-mb-16 flex w-full pl-[min(75vw)] md:pl-[min(76vw)]"
+            speed={-5}
+            className="z-10 -mb-16 -mt-12 flex w-full pl-[min(75vw)] md:mt-0 md:pl-[min(76vw)]"
           >
-            <ExpCircle />
+            <ExpCircle className="h-16 w-16 md:h-20 md:w-20" />
           </Parallax>
-          <Parallax speed={-20} className="flex w-full pl-[min(5vw)]">
-            <ExpCircle className="h-14 w-14 md:h-28 md:w-28" />
+          <Parallax
+            speed={-10}
+            className="z-10 flex w-full pl-[min(1vw)] pt-[min(4vh)] md:pl-[min(5vw)] md:pt-0"
+          >
+            <ExpCircle className="h-12 w-12 md:h-28 md:w-28" />
           </Parallax>
           <div className="absolute mb-40 flex items-center justify-center">
             <MediaQuery maxWidth={639}>
@@ -45,7 +48,7 @@ export const HomeHero: React.FC = () => {
               />
             </MediaQuery>
           </div>
-          <div className="flex w-full flex-col pt-20 text-center leading-none lg:pt-20 xl:pt-14">
+          <div className="flex w-full flex-col pt-52 text-center leading-none lg:pt-20 xl:pt-14">
             <MediaQuery maxWidth={767}>
               <Parallax speed={25} className="z-10">
                 <RevealText
@@ -70,34 +73,34 @@ export const HomeHero: React.FC = () => {
             </Parallax>
           </div>
           <Parallax
-            speed={90}
-            className="flex w-full pl-[min(85vw)] pt-[min(5vh)] md:pt-0"
+            speed={60}
+            className="flex w-full pl-[min(60vw)] pt-[min(12vh)] md:-mt-40 md:pl-[min(85vw)]"
           >
             <ExpCircle className="h-14 w-14 md:h-32 md:w-32" />
           </Parallax>
           <Parallax
-            speed={40}
-            className="flex w-full pl-[min(14vw)] md:pl-[min(20vw)]"
+            speed={60}
+            className="z-10 -mt-20 flex w-full pl-[min(6vw)] md:mt-0 md:pl-[min(20vw)]"
           >
-            <ExpCircle className="h-14 w-14" />
+            <ExpCircle className="h-20 w-20 md:h-14 md:w-14" />
           </Parallax>
           <Parallax
             speed={20}
-            className="-mb-96 flex w-full pl-[min(65vw)] pt-[min(10vw)]"
+            className="z-10 -mb-96 flex w-full pl-[min(70vw)] pt-[min(4vh)] md:pb-[min(6vh)]"
           >
-            <ExpCircle />
+            <ExpCircle className="h-16 w-16 md:h-20 md:w-20" />
           </Parallax>
           <Parallax
-            speed={40}
-            className="-mb-96 flex w-full pl-[min(20vw)] pt-[min(80vw)] md:pt-[min(20vw)]"
+            speed={50}
+            className="z-10 -mb-96 flex w-full pl-[min(30vw)] pt-[min(40vh)] md:pl-[min(25vw)] md:pt-[min(20vh)]"
           >
-            <ExpCircle className="h-14 w-14 md:h-40 md:w-40" />
+            <ExpCircle className="h-10 w-10 md:h-40 md:w-40" />
           </Parallax>
           <Parallax
             speed={30}
-            className="-mb-20 flex w-full pl-[min(90vw)] pt-[min(14vw)] md:-mb-96"
+            className="-mb-20 flex w-full pl-[min(85vw)] pt-[min(5vh)] md:-mb-96 md:pl-[min(90vw)] md:pt-[min(20vh)]"
           >
-            <ExpCircle className="h-14 w-14" />
+            <ExpCircle className="h-10 w-10 sm:h-14 sm:w-14" />
           </Parallax>
         </>
       ) : (
